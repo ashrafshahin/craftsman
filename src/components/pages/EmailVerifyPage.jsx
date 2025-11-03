547ca243e82bdb6fa87a6fecd61786ddfa7ff2f6
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2025-11-03 05:59:32 +0000

    Email verify notification page design separately and link with Redux Data to import using useSelector

--- src/components/pages/EmailVerifyPage.jsx
+++ src/components/pages/EmailVerifyPage.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
+import { FaArrowCircleLeft } from 'react-icons/fa'
+import { data, Link } from 'react-router'
+
+import { useSelector } from 'react-redux'
 
 const EmailVerifyPage = () => {
-  return (
-      <div>
-          <div>
-              <div className='bg-primary w-full h-screen flex justify-center items-center text-white font-primary font-bold '>
-                  <div className='text-center'>
+    
+    const data = useSelector(state => (state.userDetails.value))
+    console.log(data);
+    
+    return (
+        <div>
+           
+                <div className='bg-primary w-full h-screen flex justify-center items-center text-white font-primary font-bold '>
+                    <div className='text-center shadow-xl/30 shadow-white/40 py-10 px-30 rounded-2xl '>
 
-                      <p className='text-4xl mb-10 '>Please verify your email</p>
-                      <button className=' bg-white rounded-lg text-primary py-4 font-semibold text-[16px] cursor-pointer'>
-                          <Link to="/login">
-                              <div className='flex justify-center         items-center'>
-                                  <FaArrowCircleLeft className='ml-2' />
-                                  <span className='px-6'>Back to Login</span>
-                              </div>
-                          </Link>
-                      </button>
+                        <p className='text-4xl mb-10 '>Please verify your email</p>
+                        <p className='mb-10'>This is your Registerred Email : <span className='text-yellow-600 text-2xl'>{data.email}</span></p>
+                        <button className=' bg-white rounded-lg text-primary py-4 font-semibold text-[16px] cursor-pointer  hover:text-white hover:bg-gray-600'>
+                            <Link to="/login">
+                                <div className='flex justify-center         items-center'>
+                                    <FaArrowCircleLeft className='ml-2' />
+                                    <span className='px-6'>Back to Login</span>
+                                </div>
+                            </Link>
+                        </button>
 
-                  </div>
-              </div>
-          </div>
-    </div>
-  )
+                    </div>
+                </div>
+            
+        </div>
+    )
 }
 
 export default EmailVerifyPage
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,6 +8,8 @@ import { Link, useNavigate } from 'react-router'
 
 import { FaArrowCircleLeft } from "react-icons/fa";
 
+import EmailVerifyPage from './EmailVerifyPage';
+
 
 
 
@@ -51,29 +53,12 @@ const Home = () => {
         <div className='bg-primary w-full h-screen text-white'><h1>Home</h1></div>
         
         :
+        <EmailVerifyPage />
         
-        <div>
-          <div className='bg-primary w-full h-screen flex justify-center items-center text-white font-primary font-bold '>
-            <div className='text-center shadow-xl/30 shadow-white/40 py-10 px-30 rounded-2xl '>
-
-              <p className='text-4xl mb-10 '>Please verify your email</p>
-              <p className='mb-10'>This is your Registerred Email : <span className='text-yellow-600 text-2xl'>{data.email}</span></p>
-              <button className=' bg-white rounded-lg text-primary py-4 font-semibold text-[16px] cursor-pointer  hover:text-white hover:bg-gray-600'>
-                <Link to="/login">
-                  <div className='flex justify-center         items-center'>
-                    <FaArrowCircleLeft className='ml-2' />
-                    <span className='px-6'>Back to Login</span>
-                  </div>
-                </Link>
-              </button>
-
-            </div>
-          </div>
-        </div>
-  
         
         }
 
+      
     </div>
   )
 }
