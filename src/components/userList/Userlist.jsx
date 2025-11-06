1bda89f69ed257d168b9cfd336b67f2cf204efb1
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2025-11-06 06:50:22 +0000

    Read data and dynamically show Data to User List --- Done

--- src/components/userList/Userlist.jsx
+++ src/components/userList/Userlist.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import friends from "../images/friends.png"
 import { HiDotsVertical } from "react-icons/hi";
+import { getDatabase, onValue, ref } from 'firebase/database';
 
 
 const Userlist = () => {
+    const db = getDatabase()
+    const [userList, setUserList] = useState([])
+
+    // third-party api or kono database theke data Read korte
+    //  useEffect() lagbe
+    useEffect(() => {
+        const userRef = ref(db, "users/")
+        onValue(userRef, (snapshot) => {
+            const arr = []
+            // console.log(snapshot.val(), 'ldbdjbbgb');
+            // const data = snapshot.val(); // full data snapshot asbe
+            // full snapshot of data nibo na only ITEM ta nibo
+            snapshot.forEach((item) => {
+                arr.push(item.val())
+            })
+            // console.log(arr); // pore useState niye setUserList neya hoise
+            setUserList(arr)
+
+        })
+
+    }, [])
+    console.log(userList);
+    
+
     return (
         <div>
             <div className='rounded-xl px-5 py-3 font-primary shadow shadow-black/40 mb-10 '>
@@ -14,62 +39,19 @@ const Userlist = () => {
 
                 </div>
                 <div className='h-[400px] overflow-y-scroll custom-scrollbar pr-1 '>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-2 mb-3' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
-                    <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                        <img className='pr-3 mb-4' src={friends} alt="" />
-                        <div className='pr-12'>
-                            <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                            <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                        </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
-                    </div>
+                    {
+                        userList.map((user) => (
+                            <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
+                                <img className='pr-2 mb-3' src={friends} alt="" />
+                                <div className='pr-12'>
+                                    <p className='font-semibold text-lg'>{user.username}</p>
+                                    <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>{ user.email }</p>
+                                </div>
+                                <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
+                            </div> 
+                        ))
+                    }
+                    
                 </div>
             </div>
         </div>
