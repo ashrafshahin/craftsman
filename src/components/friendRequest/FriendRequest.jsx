2289b490c1741cbb01812861970a79d059d5802f
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2025-11-13 22:17:02 +0000

    Friends can send friend request includes() method used, {one ? yes : no } this system working here, onClick has been removed from 'request sent' part to disable repeated data reading...

--- src/components/friendRequest/FriendRequest.jsx
+++ src/components/friendRequest/FriendRequest.jsx
@@ -20,37 +20,37 @@ const FriendRequest = () => {
     const db = getDatabase()
 
     // Retriving data for friendRequest section ...
-    const [friendRequestList, setFriendRequestList] = useState([])
+    const [friendRequest, setFriendRequest] = useState([])
     useEffect(() => {
-        const FriendResuestRef = ref(db, "friendRequest/")
-        onValue(FriendResuestRef, (snapshot) => {
+        const friendRequestRef = ref(db, "friendRequest/")
+        onValue(friendRequestRef, (snapshot) => {
             const arr = []
             snapshot.forEach((item) => {
-                if (data?.uid == item?.val().receiverID) {
-                    arr.push(item.val()); 
+                if (data?.uid === item?.val()?.receiverID) {
+                    arr.push(item?.val()); 
                 }
                 
             });
             // sob data aikhane chole asche...
-            setFriendRequestList(arr);
+            setFriendRequest(arr);
         
         });
 
     }, [])
-    console.log(friendRequestList);
+    console.log(friendRequest);
     
     
     return (
       <div>
           <div className='rounded-xl px-5 py-3 font-primary shadow shadow-black/40 scrollbar-thin  '>
               <div className='flex justify-between items-center pb-3 '>
-                  <h2 className='font-semibold text-lg  '>Friend Request</h2>
+                  <h2 className='font-semibold text-lg text-blue-800  '>Friend Request - receiver can see this</h2>
                   <HiDotsVertical className='font-semibold text-xl' />
                   
               </div>
               <div className='h-[400px] overflow-y-scroll custom-scrollbar pr-1 '>
                     {
-                        friendRequestList.map((item) => (
+                        friendRequest.map((item) => (
                             <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25   '>
                                 <img className='pr-2 mb-3' src={friends} alt="" />
                                 <div className='pr-12'>
@@ -64,54 +64,7 @@ const FriendRequest = () => {
                         ))
                   }
                     
-                  {/* <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                        <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div>
-                  <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                      <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div>
-                  <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                      <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div>
-                  <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                      <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div>
-                  <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                      <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div>
-                  <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                      <img className='pr-3 mb-4' src={friends} alt="" />
-                      <div className='pr-12'>
-                          <p className='font-semibold text-lg'>Ashraf Shahin</p>
-                          <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                      </div>
-                      <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Accept</button>
-                  </div> */}
+                  
               </div>
           </div>
     </div>
--- src/components/userList/Userlist.jsx
+++ src/components/userList/Userlist.jsx
@@ -62,31 +62,61 @@ const Userlist = () => {
             receiverID: item.userId
             
         });
-        
-        
-
+     
     }
+    
+    // Retriving data for friendRequest section ... no Repeat data Reading.
+    const [friendRequestList, setFriendRequestList] = useState([])
+    useEffect(() => {
+        const friendRequestListRef = ref(db, "friendRequest/")
+        onValue(friendRequestListRef, (snapshot) => {
+            const arr = []
+            snapshot.forEach((item) => {
+                // "One will happen 12 or 21 - uid get merged"
+                arr.push(item?.val().receiverID + item?.val().senderID)
+
+            });
+            // sob data aikhane chole asche...
+            setFriendRequestList(arr);
+
+        });
+
+    }, [])
+    console.log(friendRequestList, "One will happen 12 or 21 - uid get merged");
+    console.log(userList, "receiver info");
+
 
     return (
         <div>
             <div className='rounded-xl px-5 py-3 font-primary shadow shadow-black/40 mb-10 '>
                 <div className='flex justify-between items-center pb-3'>
-                    <h2 className='font-semibold text-lg mr-[200px]  '>User List</h2>
+                    <h2 className='font-semibold text-lg text-blue-800 mr-[200px]  '>User List - sender can see this</h2>
                     <HiDotsVertical className='font-semibold text-xl' />
 
                 </div>
                 <div className='h-[400px] overflow-y-scroll custom-scrollbar pr-1 '>
                     {/* map kora hoise uporer userList Arrar sob data dynamically show korar jonno */}
                     {
-                        userList.map((user) => (
+                        userList.map((item) => (
                             <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
                                 <img className='pr-2 mb-3' src={friends} alt="" />
                                 <div className='pr-12'>
-                                    <p className='font-semibold text-lg'>{user.username}</p>
-                                    <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>{user.email}</p>
+                                    <p className='font-semibold text-lg'>{item?.username}</p>
+                                    <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>{item?.email}</p>
                                 </div>
-                                <button onClick={() => handleFriendRequest(user)}
-                                    className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>+</button>
+                                {/* friend request sending system */}
+                                {
+                                    friendRequestList.includes(data?.uid + item?.userId) || 
+                                    friendRequestList.includes(item?.userId + data?.uid)
+                                        ?
+                                        <button 
+                                            className='bg-primary py-1 px-3 rounded-lg text-white  text-xl'>Request Sent </button>  
+                                        :
+                                        <button onClick={() => handleFriendRequest(item)}
+                                            className='bg-primary py-1 px-3 rounded-lg text-white  text-xl'>Add Friend </button>
+
+                                }
+                                
                             </div>
                         ))
                     }
