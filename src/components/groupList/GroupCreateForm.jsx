59e6144a373cc34735219832d6ee9460d57df10a
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2025-12-03 07:55:41 +0000

    Group database create, show data on group list  Display, condition to hide groupAdmin ... form field empty not done... work in progress...

--- src/components/groupList/GroupCreateForm.jsx
+++ src/components/groupList/GroupCreateForm.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
+
+import React, { useEffect, useState } from 'react'
+
+import { useSelector } from 'react-redux'
+
+import { getDatabase, onValue, push, ref, set } from 'firebase/database'
+import { useNavigate } from 'react-router'
 
 const GroupCreateForm = () => {
+    //navigate page 
+    const navigate = useNavigate()
 
-    
+    const db = getDatabase()
+
+    //Get data from Redux
+    const data = useSelector((selector) => (selector.userDetails.value))
+
     const [groupName, setGroupName] = useState(" ")
     const [groupNameError, setGroupNameError] = useState(" ")
 
@@ -11,12 +23,17 @@ const GroupCreateForm = () => {
 
     const [groupTagline, setGroupTagline] = useState(" ")
     const [groupTaglineError, setGroupTaglineError] = useState(" ")
- 
+
+    const [loading, setLoading] = useState(false)
+
+    const [show, setShow] = useState(false)
+
     const handleCreateGroup = () => {
+        
         console.log(groupName, 'error keno ase na');
         console.log(groupCreator, 'error');
         console.log(groupTagline, 'error');
-       
+
         if (!groupName.trim()) {
             console.log('Group Name is Required');
             setGroupNameError('Group Name is Required')
@@ -28,60 +45,105 @@ const GroupCreateForm = () => {
         if (!groupTagline.trim()) {
             console.log('tagline dete hobe');
             setGroupTaglineError('Group tagline is Required')
-            
+
         }
-        
-        
-        
+
+        // Writing/push data to database ChtDay-13
+        if (groupName && groupCreator && groupTagline) {
+            setLoading(true)
+            set(push(ref(db, 'groupList')), {
+                groupName: groupName,
+                groupCreator: groupCreator,
+                groupTagline: groupTagline,
+                groupAdmin: data?.uid,
+            });
+            // setTimeout(() => {
+            //     // navigate("/")
+            // }, 3000)
+            setLoading(false)
+            // after group create fields will be empty bcoz of these codes ..
+            setGroupName('')
+            setGroupCreator('')
+            setGroupTagline('')
+            /// windows reload...
+            setTimeout(() => {
+                window.location.reload()
+            }, 5000)
+        }
+      
     }
+
+    // // Retriving/ Get data towards -> Group List section ... no Repeat data Reading.
+    //     const [groupList, setGroupList] = useState([])
+    //     useEffect(() => {
+    //         const groupListRef = ref(db, "groupList/")
+    //         onValue(groupListRef, (snapshot) => {
+    //             const arr = []
+    //             snapshot.forEach((item) => {
+    //                 arr.push(item?.val())
     
+    //             });
+    //             // sob data aikhane chole asche...
+    //             setGroupList(arr);
+    
+    //         });
+    
+    //     }, [])
+    // console.log(groupList);
+    
+
     const handleGoBack = () => {
-        
+        setTimeout(() => {
+            navigate("/allGroups")
+        }, 3000)
     }
 
     return (
-      <div>
-          <div className='font-primary text-center'>
-              <div>
-                  <h3 className='md:w-[497px] font-bold font-second text-[25px] md:text-[34px] text-[#f2f3f6] '>Group Create Form</h3>
-                  
-              </div>
-              <h4 className='text-[20px] p-5 font-bold text-[#61c2ef]'>Create your own Group and Enjoy it for Free</h4>
-              <div>
+        <div>
+            <div className='md:w-full'>
+                <div className=' font-primary text-center'>
                     <div>
+                        <h3 className='w-100 md:w-[497px] font-bold font-second text-[20px] md:text-[34px] text-[#f2f3f6] '>Group Create Form</h3>
+
+                    </div>
+                    <h4 className='md:text-[20px] p-5 font-bold text-[#61c2ef]'>Create your own Group and Enjoy it for Free</h4>
+                    <div>
+                        <div>
+
+                            <input onChange={(e) => (setGroupName(e.target.value), setGroupNameError(''))}
+                                className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Name' />
+
+
+                            <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupNameError}</p>
+
+                        </div>
+                        <div className='py-8'>
+                            <input onChange={(e) => (setGroupCreator(e.target.value), setGroupCreatorError(''))}
+                                className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Creator' />
+                            <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupCreatorError}</p>
+
+                        </div>
+                        <div>
+                            <input onChange={(e) => (setGroupTagline(e.target.value), setGroupTaglineError(''))}
+                                className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Tagline' />
+                            <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupTaglineError}</p>
+
+                        </div>
+                    </div>
+                    <div className='py-5'>
                         
-                        <input onChange={(e) => (setGroupName(e.target.value), setGroupNameError(''))}
-                            className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Name' />
-                        
+                        <button onClick={handleCreateGroup}
+                            className=' w-full bg-primary hover:bg-green-800  rounded-xl text-white py-5 font-second font-semibold text-[20px] cursor-pointer'>Create Group</button>
+                        <button onClick={handleGoBack}
+                            className='bg-red-500 hover:bg-green-800 rounded-lg text-white py-2 px-4 cursor-pointer mt-10'>Go Back</button>
 
-                        <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupNameError}</p>
+                    </div>
 
-                  </div>
-                  <div className='py-8'>
-                        <input onChange={(e) => (setGroupCreator(e.target.value), setGroupCreatorError(''))}
-                            className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Creator' />
-                        <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupCreatorError}</p>
+                </div>
 
-                  </div>
-                  <div>
-                        <input onChange={(e) => (setGroupTagline(e.target.value), setGroupTaglineError(''))}
-                            className='w-full border-2 text-[#ffffff] py-[20px] pl-[52px] pr-[66px] rounded-[9px] ' type="text" placeholder='Group Tagline' />
-                        <p className='w-full bg-red-600 text-center rounded-full text-white px-3 mt-2 font-second font-semibold text-[15px]'>{groupTaglineError}</p>
-
-                  </div>
-              </div>
-              <div className='py-5'>
-                    <button onClick={handleCreateGroup}
-                        className=' w-full bg-primary hover:bg-green-800  rounded-xl text-white py-5 font-second font-semibold text-[20px] cursor-pointer'>Create Group</button>
-                    <button onClick={handleGoBack}
-                        className='bg-red-500 hover:bg-green-800 rounded-lg text-white py-2 px-4 cursor-pointer mt-10'>Go Back</button>
-                            
-              </div>
-              
-          </div>
-          
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default GroupCreateForm
--- src/components/groupList/Grouplist.jsx
+++ src/components/groupList/Grouplist.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { useSelector } from 'react-redux'
+
+import { getDatabase, onValue, push, ref, set } from 'firebase/database'
 
 import friends from "../images/friends.png"
 import { HiDotsVertical } from "react-icons/hi";
@@ -9,71 +13,96 @@ import GroupCreateForm from './GroupCreateForm';
 
 
 const GroupList = () => {
-    
+    const db = getDatabase()
+
+    //Get data from Redux
+    const data = useSelector((selector) => (selector.userDetails.value))
+
     const [show, setShow] = useState(false)
-    
+
     const handleGroup = () => {
         setShow(!show)
     }
 
-  return (
-      <div>
-          <div className=' rounded-xl px-5 py-3 font-primary shadow shadow-black/40 scrollbar-thin mb-10  '>
-              <div className='flex justify-between items-center pb-3 '>
-                  <h2 className='font-semibold text-lg  '>Groups List</h2>
-                  {/* <HiDotsVertical className='font-semibold text-xl' /> */}
-                  <div>
-                      <div>
-                         
-                          {
-                              show ? 
-                                  <button onClick={handleGroup}
-                                      className='bg-red-500 rounded-lg text-white py-2 px-4'>Go Back</button>
-                                  :
-                                  <button onClick={handleGroup}
-                                      className='bg-green-600 rounded-lg text-white py-2 px-4'>Create Group</button>
-                          }
-                          
-                      </div>
-                  </div>
-                  
-              </div>
-              {
-                  show ?
-                      <div className='relative'>
-                          <div className='absolute top-0 left-40  bg-gray-800/90 rounded-2xl px-50 py-30  z-[1]'>
-                              <GroupCreateForm />
-                              
-                          </div>
-                      </div>
-                      
-                      : 
-                      <div className='h-[400px] overflow-y-scroll custom-scrollbar pr-1 '>
+    // Retriving/ Get data towards -> Group List section ... no Repeat data Reading.
+    const [groupList, setGroupList] = useState([])
+    useEffect(() => {
+        const groupListRef = ref(db, "groupList/")
+        onValue(groupListRef, (snapshot) => {
+            const arr = []
+            snapshot.forEach((item) => {
+                if (item.val().groupAdmin != data.uid) {
+                    arr.push(item?.val())
+                }
 
-                          <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25   '>
-                              <img className='pr-2 mb-3' src={friends} alt="" />
-                              <div className='pr-12'>
-                                  <p className='font-semibold text-lg'>Shahin</p>
-                                  <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                              </div>
-                              <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Join</button>
-                          </div>
-                          <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25  '>
-                              <img className='pr-3 mb-4' src={friends} alt="" />
-                              <div className='pr-12'>
-                                  <p className='font-semibold text-lg'>Shahin</p>
-                                  <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>Hi Guys, Wassup!</p>
-                              </div>
-                              <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Join</button>
-                          </div>
+            });
+            // sob data aikhane chole asche...
+            setGroupList(arr);
 
-                      </div>
-                      
-                  }
-              
-          </div>
-    </div>
-  )
+        });
+
+    }, [])
+    console.log(groupList);
+
+    return (
+        <div>
+            <div className=' rounded-xl md:px-5 md:py-3 font-primary shadow shadow-black/40 scrollbar-thin mb-10  '>
+                <div className='flex justify-between items-center pb-3 '>
+                    <h2 className='font-semibold text-lg  '>Groups List</h2>
+                    {/* <HiDotsVertical className='font-semibold text-xl' /> */}
+                    <div>
+                        <div>
+
+                            {
+                                show ?
+                                    <button onClick={handleGroup}
+                                        className='bg-red-500 rounded-lg text-white py-2 px-4'>Go Back</button>
+                                    :
+                                    <button onClick={handleGroup}
+                                        className='bg-green-600 rounded-lg text-white py-2 px-4'>Create Group</button>
+                            }
+
+                        </div>
+                    </div>
+
+                </div>
+                {
+                    show ?
+                        <div className='relative'>
+                            <div className='absolute top-0 left-40  bg-gray-800/90 rounded-2xl px-20 md:px-50 py-10 md:py-30  z-[1]'>
+                                <GroupCreateForm />
+
+                            </div>
+                        </div>
+
+                        :
+                        <div>
+                            {
+                                groupList.map((item) => (
+                                    <div className=' overflow-y-scroll custom-scrollbar pr-1 '>
+
+                                        <div className='flex justify-between items-center mt-4 border-b-2 border-b-black/25   '>
+                                            <img className='pr-2 mb-3' src={friends} alt="" />
+                                            <div className='pr-12'>
+                                                <p className=' text-sm'> {item.groupCreator},<span className='text-blue-800'>Admin</span></p>
+                                                <p className='font-semibold text-sm'>{item.groupName}</p>
+                                                <p className='font-medium text-sm text-[rgba(77,77,77,0.75)] '>{item.groupTagline}</p>
+                                            </div>
+                                            <button className='bg-primary py-1 px-5 rounded-lg text-white font-semibold text-xl'>Join</button>
+                                        </div>
+
+
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+
+                }
+
+            </div>
+        </div>
+    )
 }
 
 export default GroupList
